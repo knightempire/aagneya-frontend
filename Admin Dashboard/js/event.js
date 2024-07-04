28bd639f737411e23b019397ad60f6e8a2f9bf5a
Abi <knightempire24@gmail.com>
2024-07-04 22:46:11 +0530

    event dispaying

--- Admin Dashboard/js/event.js
+++ Admin Dashboard/js/event.js
@@ -1,18 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Get all toggleReadMore elements
-    const toggleReadMoreLinks = document.querySelectorAll('.toggleReadMore');
-
-    toggleReadMoreLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            const readMoreContent = this.closest('.card').querySelector('.readMoreContent');
-            readMoreContent.classList.toggle('open');
-            this.textContent = readMoreContent.classList.contains('open') ? 'Read less' : 'Read more';
-        });
-    });
-});
-
-
 function showEditForm() {
     // Hide the element with id="displayevents"
     var displayEvents = document.getElementById('displayevents');
