b3c288bd205d6fb0eaa9437ca2ab05257ffef874
Leonallr10 <118210551+Leonallr10@users.noreply.github.com>
2024-07-05 00:08:32 +0530

    console addqa script

--- login/js/add_qa.js
+++ login/js/add_qa.js
@@ -1,38 +1,53 @@
 $(document).ready(function() {
-    var $steps = $('.step');
-    var $activeLine = $('.active-line');
-    var totalSteps = $steps.length;
-    var currentStep = 1;
-    var sliderWidth = $('.steps-container').width();
+  var $steps = $('.step');
+  var $activeLine = $('.active-line');
+  var totalSteps = $steps.length;
+  var currentStep = 1;
+  var sliderWidth = $('.steps-container').width();
 
-    $('.next').click(function(e) {
-      e.preventDefault();
-      var $currentForm = $(this).closest('.slider-form');
-      var $nextForm = $currentForm.next('.slider-form');
+  // Object to store form data
+  var formData = {};
 
-      // Check if the input field is valid
-      if ($currentForm.find('input[required]').val() === '') {
-        $currentForm.find('.validation-message').fadeIn().delay(2000).fadeOut();
-        return;
-      }
+  $('.next').click(function(e) {
+    e.preventDefault();
+    var $currentForm = $(this).closest('.slider-form');
+    var $nextForm = $currentForm.next('.slider-form');
 
-      if ($nextForm.length > 0) {
-        $currentForm.removeClass('active');
-        $nextForm.addClass('active');
-        currentStep++;
-        updateSlider();
-      }
-    });
-
-    function updateSlider() {
-      var stepWidth = (currentStep - 1) * (sliderWidth / (totalSteps - 1));
-      $activeLine.css('width', stepWidth + 'px');
-      $steps.removeClass('active');
-      $steps.eq(currentStep - 1).addClass('active');
+    // Check if the input field is valid
+    var $inputField = $currentForm.find('input[required]');
+    if ($inputField.val() === '') {
+      $currentForm.find('.validation-message').fadeIn().delay(2000).fadeOut();
+      return;
     }
 
-    $(window).resize(function() {
-      sliderWidth = $('.steps-container').width();
+    // Store input value in formData object
+    var inputName = $inputField.attr('placeholder');
+    formData[inputName] = $inputField.val();
+
+    if ($nextForm.length > 0) {
+      $currentForm.removeClass('active');
+      $nextForm.addClass('active');
+      currentStep++;
       updateSlider();
-    });
+    }
+
+    // Log formData to console
+    console.log(formData);
   });
+
+  $('.submit').click(function() {
+    console.log('Final form data:', formData);
+  });
+
+  function updateSlider() {
+    var stepWidth = (currentStep - 1) * (sliderWidth / (totalSteps - 1));
+    $activeLine.css('width', stepWidth + 'px');
+    $steps.removeClass('active');
+    $steps.eq(currentStep - 1).addClass('active');
+  }
+
+  $(window).resize(function() {
+    sliderWidth = $('.steps-container').width();
+    updateSlider();
+  });
+});
